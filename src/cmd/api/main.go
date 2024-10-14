cfdf47ff4524986266fac6c69b8094c19ad1b817
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-14 20:36:35 +0530

    Update main.go

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
+const versionNumber = "1.0.1"
+
+type serverConfig struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config serverConfig
+	logger *log.Logger
+}
+
 func main() {
+	var cfg serverConfig
+
+	flag.IntVar(&cfg.port, "port", 8080, "Port to run the server on")
+	flag.StringVar(&cfg.env, "env", "Development", "Environment the server is running in")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/api/v1/healthcheck", healthcheck)
 	mux.HandleFunc("/api/v1/version", version)
 
-	fmt.Println("Starting ... Server on port 8080")
-	err := http.ListenAndServe(":8080", mux)
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	addr := fmt.Sprintf(":%d", cfg.port)
+
+	logger.Printf("Starting %s server on %s", cfg.env, addr)
+	err := http.ListenAndServe(addr, mux)
 	if err != nil {
 		fmt.Println("Error starting server: ", err)
 	}
@@ -34,5 +64,5 @@ func version(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Environment: %s\n", "Development")
-	fmt.Fprintf(w, "Version: %s\n", "1.0.1")
+	fmt.Fprintf(w, "Version: %s\n", versionNumber)
 }
