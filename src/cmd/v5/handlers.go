ccf026db9b0d901e526037115c24efb6503c8eb5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-15 15:41:34 +0530

    Two More Routes

--- src/cmd/v5/handlers.go
+++ src/cmd/v5/handlers.go
@@ -31,3 +31,21 @@ func (app *application) VersionInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "Version: %s\n", version)
 }
+
+func (app *application) getBooksHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintln(w, "Displaying all books")
+}
+
+func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintln(w, "New Book Created")
+}
--- src/cmd/v5/routes.go
+++ src/cmd/v5/routes.go
@@ -8,8 +8,12 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/", http.NotFound) // Catch-all route
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/helloworld", HelloWorld)
+
 	mux.HandleFunc("/api/v1/healthcheck", app.HealthCheck)
 	mux.HandleFunc("/api/v1/version", app.VersionInfo)
+
+	mux.HandleFunc("/api/v1/books", app.getBooksHandler)
+	mux.HandleFunc("/api/v1/books", app.createBookHandler)
 
 	return mux
 }
