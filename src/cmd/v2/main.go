f1d02e08881628522d7a6174d5941409b4aab483
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-15 11:51:18 +0530

    Version 2 - verified

--- src/cmd/v2/main.go
+++ src/cmd/v2/main.go
@@ -14,6 +14,31 @@ func main() {
 		fmt.Fprintf(w, "Hello Welcome to Go Lang World, you've requested: %s\n", r.URL.Path)
 	})
 
+	http.HandleFunc("/api/v1/healthcheck", healthcheck)
+	http.HandleFunc("/api/v1/version", versioninfo)
+
 	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil) // DefaultServeMux.
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintln(w, "Status: Healthy")
+}
+
+func versioninfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintf(w, "Environment: %s\n", "Development")
+	fmt.Fprintf(w, "Version: %s\n", "1.0.1")
 }
