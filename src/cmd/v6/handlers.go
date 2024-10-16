4f98f95c2f146d62a23b5bf44e93078f62631523
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-16 21:13:24 +0530

    Verified Get All Books

--- src/cmd/v6/handlers.go
+++ src/cmd/v6/handlers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"go_books_store_api/cmd/v6/data"
 	"net/http"
+	"time"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -67,5 +69,48 @@ func (app *application) getBooksHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	fmt.Fprintln(w, "Displaying all books")
+	books := getDummyBooks()
+
+	js, err := json.Marshal(books)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(js)
+}
+
+func getDummyBooks() []data.Book {
+	return []data.Book{
+		{
+			ID:        1,
+			CreatedAt: time.Now(),
+			Title:     "The Darkening of Tristram",
+			Published: 1998,
+			Pages:     300,
+			Genres:    []string{"Fiction", "Thriller"},
+			Rating:    4.5,
+			Version:   1,
+		},
+		{
+			ID:        2,
+			CreatedAt: time.Now(),
+			Title:     "The Legecy of Deckard Cain",
+			Published: 2007,
+			Pages:     432,
+			Genres:    []string{"Fiction", "Adventure"},
+			Rating:    4.9,
+			Version:   1,
+		},
+		{
+			ID:        3,          // system generated
+			CreatedAt: time.Now(), // system generated
+			Title:     "The Black Soulstone",
+			Version:   1, // system generated
+		},
+	}
 }
